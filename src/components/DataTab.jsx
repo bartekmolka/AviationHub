5524a0c0b72b646208f09d0a88f584d4729403b1
n00sek <74304320+n00sek@users.noreply.github.com>
2022-06-09 09:52:33 +0200

    Update DataTab.jsx

--- src/components/DataTab.jsx
+++ src/components/DataTab.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Table from 'react-bootstrap/Table'
 
 export default function DataTab({data}) {
+
+    const dane = [...data]
+    console.log(dane)
     return (
         <div>
         <Table striped bordered hover variant="light" responsive>
-            {data ?
+            {dane ?
             <>
                 <thead>
                     <tr>
-                        {Object.keys(data[0] || []).map((e, i) => <th key={i}>{e}</th>)}
+                        {Object.keys(dane[0] || []).map((e, i) => <th key={i}>{e}</th>)}
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((e, i) => <tr key={i}>{Object.values(e).map((e, i) => <td key={i}>{e}</td>)}</tr>)}
+                    {dane.forEach((e, i) => <tr key={i}>{Object.values(e).map((e, i) => <td key={i}>{e}</td>)}</tr>)}
                 </tbody>
             </>
                 :
