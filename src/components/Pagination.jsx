de298fc5501e8a72beff431acdfb12ffb0bb9206
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2022-06-09 12:40:56 +0200

    Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,10 +4,12 @@ import { useSearchParams } from 'react-router-dom'
 import GetData from "./GetData";
 import * as styles from '../styles/Pagination.module.css'
 
+const baseUrl = '/data?p='
+
 export default function PaginationContainer({ children, tabs, }) {
     const [searchParams, setSearchParams] = useSearchParams();
     const p = searchParams.get("p")
-    let active = 2;
+    let active = parseInt(p.toString()) || 1;
     let items = [];
     for (let number = 1; number <= tabs; number++) {
         items.push(
@@ -24,13 +26,11 @@ export default function PaginationContainer({ children, tabs, }) {
             {children}
             <div className={styles.searching}>
                 <Pagination>
-                    <Pagination.First />
-                    <Pagination.Prev />
-                    <Pagination.Ellipsis />
-                    <Pagination active>{items[p - 1]}</Pagination>
-                    <Pagination.Ellipsis />
-                    <Pagination.Next />
-                    <Pagination.Last />
+                    <Pagination.First href={baseUrl + 1} />
+                    <Pagination.Prev disabled={active <= 0} href={baseUrl + (active - 1)}/>
+                    <Pagination.Item active>{active - 1}</Pagination.Item>
+                    <Pagination.Next disabled={active >= tabs} href={baseUrl + (active + 1)}/>
+                    <Pagination.Last href={baseUrl + (tabs - 1)}/>
                 </Pagination>
             </div>
         </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap"
 
 export default function Main() {
   return (
     <div>
-      <h1>Strona główna</h1>
+      <h1 className="mx-auto">Strona główna</h1>
+      <Card className="mx-auto" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://as2.ftcdn.net/v2/jpg/02/71/78/29/1000_F_271782927_keMVFo9PnBwrMEmbiUGKRcDT2rzf85dj.jpg" />
+        <Card.Body>
+          <Card.Title>Aviation hub</Card.Title>
+          <Card.Text>
+            Simpleest way to find nearest airport
+          </Card.Text>
+          <Button variant="primary"><a className="text-reset" href="/data">Search for airport</a></Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }   
